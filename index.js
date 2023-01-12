673e9c5e787626390449e9434207cb3397068feb
Sara McDonnell <saramcdonnell32@gmail.com>
2023-01-12 18:11:29 -0500

    lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickUpLocation) {
+  const scuberHeadquartersBlock = 42;
+  return Math.abs(pickUpLocation - scuberHeadquartersBlock);
+}
+
+function distanceFromHqInFeet(pickUpLocation) {
+  return distanceFromHqInBlocks(pickUpLocation) * 264;
+}
+
+function distanceTravelledInFeet(startingBlock, endingBlock) {
+  return Math.abs(startingBlock - endingBlock) * 264;
+}
+
+function calculatesFarePrice(startingBlock, endingBlock) {
+  const distanceInFeet = distanceTravelledInFeet(startingBlock, endingBlock);
+  let price;
+  if (distanceInFeet <= 400) {
+    price = 0;
+  } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+    price = (distanceInFeet - 400) * 0.02;
+  } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+    price = 25;
+  } else price = "cannot travel that far";
+  return price;
+}
